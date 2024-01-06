d3558decd8627ad961a06264c48a94667ff5cac8
Kazi Rifat Morshed <kazirifat03.krm@gmail.com>
2024-01-06 19:36:52 +0600

    123

---
+++ idontknowjava.java
@@ -0,0 +1,8 @@
+public class idontknowjava {
+    public static void main(String[] args) {
+        System.out.print("believe me bro\n");
+        System.out.println("I Don't know java");
+        System.out.print("I like to call it vaja");
+    }
+}
+// java তুমি এতো ব্যাকা ত্যাড়া কেন, এত্তো বেশি নাট বল্টু কেন লাগে
