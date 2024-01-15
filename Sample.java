f6d8300c425afd75a89b86feb48c1f2293d3eed6
Kazi Rifat Morshed <kazirifat03.krm@gmail.com>
2024-01-16 01:57:28 +0600

    aaaa

--- Sample.java
+++ Sample.java
@@ -1,10 +1,12 @@
+/*
 public class idontknowjava {
-    public static void main(String[] args) {
-        System.out.print("believe me bro\n");
-        System.out.println("I Don't know java");
-        System.out.print("I like to call it vaja");
+        public static void main(String[] args) {
+            System.out.print("believe me bro\n");
+            System.out.println("I Don't know java");
+            System.out.print("I like to call it vaja");
+        }
     }
-}
+ */
 // java তুমি এতো ব্যাকা ত্যাড়া কেন, এত্তো বেশি নাট বল্টু কেন লাগে
 /*
 /usr/lib/jvm/java-21-openjdk/bin/java -javaagent:/mnt/IDEs/idea-IU-232.9921.47/lib/idea_rt.jar=44609:/mnt/IDEs/idea-IU-232.9921.47/bin -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath /mnt/GitHub/LearningJava/out/production/LearningJava idontknowjava
@@ -13,3 +15,8 @@ I Don't know java
 I like to call it vaja
 Process finished with exit code 0
  */
+public class Sample{ // it suggested me to rename the file
+    public static void main(String []args){
+        System.out.println("MEAW");
+    }
+}
