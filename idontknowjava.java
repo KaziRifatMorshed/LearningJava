d196b49b778fb4b74e1ae8a3603f36f507cc5907
Kazi Rifat Morshed <kazirifat03.krm@gmail.com>
2024-01-16 01:14:31 +0600

    ah

--- idontknowjava.java
+++ idontknowjava.java
@@ -6,3 +6,10 @@ public class idontknowjava {
     }
 }
 // java তুমি এতো ব্যাকা ত্যাড়া কেন, এত্তো বেশি নাট বল্টু কেন লাগে
+/*
+/usr/lib/jvm/java-21-openjdk/bin/java -javaagent:/mnt/IDEs/idea-IU-232.9921.47/lib/idea_rt.jar=44609:/mnt/IDEs/idea-IU-232.9921.47/bin -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath /mnt/GitHub/LearningJava/out/production/LearningJava idontknowjava
+believe me bro
+I Don't know java
+I like to call it vaja
+Process finished with exit code 0
+ */
