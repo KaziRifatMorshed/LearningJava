ecc8a5aebc58521473cf722919e7eaf4610a36bb
Kazi Rifat Morshed <kazirifat03.krm@gmail.com>
2024-02-13 01:30:11 +0600

    13Feb0146AM

--- Point/Point.java
+++ Point/Point.java
@@ -1,19 +1,15 @@
 // Kazi Rifat Morshed 230220
 package Point;
-
 public class Point {
     private double x;
     private double y;
-
     public Point(double _x, double _y){
         this.x = _x;
         this.y = _y;
     }
-
     public double distance(Point second){
         return Math.sqrt( Math.pow((x - second.x), 2) + Math.pow((y - second.y), 2) );
     }
-
     public String toString() {
         return STR."location is (\{x},\{y})";
     }
--- account/Account.java
+++ account/Account.java
@@ -1,10 +1,8 @@
 package account;
 import java.text.NumberFormat;
-
 public class Account
 {
   private final double RATE = 0.035;  // interest rate of 3.5%
-  
   private long acctNumber;
   private double balance;
   private String name;
@@ -16,10 +14,9 @@ public class Account
     balance = initial;
   }
 
-  public double deposit (double amount)  { // balance add kora
-    if (amount > 0)
+  public void deposit (double amount)  { // balance add kora
+    if (amount > 0) // good practice
       balance = balance + amount;
-    return balance;
   }
 
   public double getBalance ()
@@ -27,29 +24,28 @@ public class Account
     return balance;
   }
 
-  public double withdraw (double amount)  {
+  public void withdraw (double amount)  {
     if (amount <= balance)
       balance = balance - amount ;
-    return balance;
   }
 
   public void Transfer(Account to){
     to.balance = to.balance + balance;
     balance = 0;
-      System.out.println(STR."Balance has been transferred successfully.");
+      System.out.println("Balance has been transferred successfully.");
   }
 
   public void Transfer_V2(Account a){
     a.deposit(balance); // per er vitor je ache tar $ ke add kore debo
     balance = 0;
-    System.out.println(STR."Balance has been transferred successfully.");
+    System.out.println("Balance has been transferred successfully.");
   }
 
   public String toString ()
   {
     NumberFormat fmt = NumberFormat.getCurrencyInstance();
     //return (acctNumber + "\t" + name + "\t" + balance.NumberFormat.getCurrencyInstance());
-    return ("Acc Number: " + acctNumber + "\t" + "Acc Name: " + name + "\t" + "Balance: " + fmt.format(balance));
+    return (STR."Acc Number: \{acctNumber}\tAcc Name: \{name}\tBalance: \{fmt.format(balance)}");
   }
 }
 
