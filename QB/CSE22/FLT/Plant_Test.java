9f2b12f62015fd1353448200573e8f44c5827cb3
Kazi Rifat Morshed <kazirifat03.krm@gmail.com>
2024-04-02 05:17:00 +0600

    02April0517AM
    CT test ques solved, hurrah !!!

--- QB/CSE22/FLT/Plant_Test.java
+++ QB/CSE22/FLT/Plant_Test.java
@@ -1,28 +1,5 @@
 package QB.CSE22.FLT;
 
-public class Plant_Test {
-
-    public static void main(String[] args) {
-        Plant[] garden = new Plant[3];
-        garden[0] = new Plant("Vine");
-        garden[1] = new Lotus("Vanity");
-        Lotus l = new Lotus("Pride", 2048);
-        l.smellOn();
-        garden[2] = l;
-
-        for (Plant p : garden) {
-            System.out.println(p);
-        }
-        System.out.println("There are " + Plant.getNumPlants()
-                + " plants.");
-
-        Lotus r = new Lotus("Loathing");
-        System.out.println(r.getName());
-        System.out.println("There are " + Lotus.getNumLotuses()
-                + " Lotus plants.");
-    }
-}
-
 /*
 * Implement the missing Lotus class so that when we run the code above it will print:
 
@@ -35,3 +12,26 @@ There are 3 Lotus plants.
 
   Note: Lotus must implement Smell interface
 * */
+public class Plant_Test {
+    public static void main(String[] args) {
+        Plant[] garden = new Plant[3];
+        garden[0] = new Plant("Vine");
+        garden[1] = new Lotus("Vanity");
+        Lotus l = new Lotus("Pride", 2048);
+        l.smellOn(); // will change a instantiate bool value
+        garden[2] = l;
+
+        for (Plant p : garden) {
+            System.out.println(p);
+        }
+
+        System.out.println(STR."There are \{Plant.getNumPlants()} plants."); // There are 3 plants.
+
+        Lotus r = new Lotus("Loathing"); //
+        System.out.println(r.getName()); // Lotus:Loathing has 1024 leaves
+        System.out.println(STR."There are \{Lotus.getNumLotuses()} Lotus plants."); // There are 3 Lotus plants.
+        // static LOTUS e
+    }
+}
+
+
