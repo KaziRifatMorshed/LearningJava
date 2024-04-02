2682c576451dbaa32e7686763d5b6d3c8c3c9dbc
Kazi Rifat Morshed <kazirifat03.krm@gmail.com>
2024-04-02 09:21:57 +0600

    6486456454561

---
+++ Exceptions/Finally.java
@@ -0,0 +1,20 @@
+package Exceptions;
+
+public class Finally {
+    public static void main(String[] args) {
+        int numerator = 10;
+        int denominator = 0;
+        try {
+            System.out.println(numerator / denominator);
+            System.out.println("This text will not be printed.");
+        } catch (java.lang.ArithmeticException e) {
+            System.out.println("(java.lang.ArithmeticException) This text will be printed.");
+        } finally {
+            System.out.println("FINALLY, This text will also be printed.");
+        }
+    }
+}
+/*
+(java.lang.ArithmeticException) This text will be printed.
+FINALLY, This text will also be printed.
+* */
