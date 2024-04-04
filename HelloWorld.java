6118d7319716bed5af6b8c857658db2f2cc69127
Kazi Rifat Morshed <kazirifat03.krm@gmail.com>
2024-04-04 20:22:38 +0600

    xxxxxx

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,7 +6,8 @@ class HelloWorld{
         String m1, m2, m3;
         m1 = "Quest for the Holy Grail";
         m2 = m1.toLowerCase();
-        m3 = STR."\{m1} \{m2}";
-        System.out.println (m3.replace('h','z'));
+        //m3 = STR."\{m1} \{m2}";
+        // System.out.println (m3.replace('h','z'));
+        System.out.println (m2);
     }
 }
