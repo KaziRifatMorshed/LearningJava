716dbaa847e56a00aa4372ae9b9b6a77ff1f03c9
Kazi Rifat Morshed <kazirifat03.krm@gmail.com>
2024-04-04 22:01:00 +0600

    0

--- GUI/JFrame_1.java
+++ GUI/JFrame_1.java
@@ -1,4 +1,11 @@
 package GUI;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class JFrame_1 {
+    public static void main(String[] args) {
+        MyFrame myFrame1 = new MyFrame();
+        myFrame1.setSize(300, 300); // default e 480x480 te on hobe
+    }
 }
---
+++ GUI/MyFrame.java
@@ -0,0 +1,19 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MyFrame extends JFrame {
+    public MyFrame(){
+        // JFrame this = new JFrame();
+        this.setSize(480,480);
+        this.setVisible(true);
+        this.setTitle("Disco Club");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        ImageIcon img1_logo = new ImageIcon("answer.svg"); // ImageIcon
+        this.setIconImage(img1_logo.getImage()); // img1_logo.getImage()
+
+        this.getContentPane().setBackground(Color.YELLOW); // getContentPane()
+    }
+}
