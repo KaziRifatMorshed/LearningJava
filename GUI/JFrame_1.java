830c231c7c925c96abe8b1c3a4577107f0fe7cee
Kazi Rifat Morshed <kazirifat03.krm@gmail.com>
2024-04-04 22:04:08 +0600

    645

--- GUI/JFrame_1.java
+++ GUI/JFrame_1.java
@@ -7,5 +7,6 @@ public class JFrame_1 {
     public static void main(String[] args) {
         MyFrame myFrame1 = new MyFrame();
         myFrame1.setSize(300, 300); // default e 480x480 te on hobe
+        
     }
 }
