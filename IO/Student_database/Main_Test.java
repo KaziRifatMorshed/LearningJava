e246a4c5d3ca18b32a2ad07b39bab6cb86c20966
Kazi Rifat Morshed <kazirifat03.krm@gmail.com>
2024-04-10 16:58:55 +0600

    68

--- IO/Student_database/Main_Test.java
+++ IO/Student_database/Main_Test.java
@@ -3,6 +3,11 @@ package IO.Student_database;
 public class Main_Test {
     public static void main(String[] args) {
         // Main class
-
+        _Address ad = new _Address("Hall Road", "Batiyaghata", "Khulna");
+        _Person p = new _Person("Anis", ad);
+        _Person s = new _Student(200201, "Siam", ad);
+        System.out.println(p);
+        System.out.println("----------------------");
+        System.out.println(s);
     }
 }
