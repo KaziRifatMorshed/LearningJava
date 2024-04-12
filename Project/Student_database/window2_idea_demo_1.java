97cf9cae312d8f26052813c94ddd19d8cd407700
Kazi Rifat Morshed <kazirifat03.krm@gmail.com>
2024-04-12 13:05:57 +0600

    sukafgvuilasg684fl

---
+++ Project/Student_database/window2_idea_demo_1.java
@@ -0,0 +1,25 @@
+package Project.Student_database;
+
+import javax.swing.*;
+
+public class window2_idea_demo_1 {
+    private JPanel windowTwo_IDEA_demo_1;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    protected JFrame windoW2 = null;
+
+    public window2_idea_demo_1() {
+        windoW2 = new JFrame();
+        windoW2.setTitle("daTa maNipulation Form");
+        windoW2.setSize(500, 500);
+        windoW2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        windoW2.setVisible(true);
+        windoW2.setContentPane(windowTwo_IDEA_demo_1);
+    }
+
+    public static void main(String[] args) {
+        new window2_idea_demo_1();
+    }
+}
