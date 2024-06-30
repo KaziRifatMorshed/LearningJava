e623d0e0b9849353f7d074e195ec9a831370e800
Kazi Rifat Morshed <kazirifat03.krm@gmail.com>
2024-06-30 16:22:13 +0600

    0

--- TelegramBot/demoFirst.java
+++ TelegramBot/demoFirst.java
@@ -4,10 +4,12 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+
 public class demoFirst {
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner scanner = new Scanner(new File("~/tbtKU.txt"));
+        Scanner scanner = new Scanner(new File("/home/noobcoder_rtx/tbtKU.txt"));
         String text = scanner.nextLine();
-        System.out.println(text);
+
+
     }
 }
