88e9260a28a0ee7f0be1c7096b0c890820d870bb
Kazi Rifat Morshed <kazirifat03.krm@gmail.com>
2024-08-24 21:10:11 +0600

    0

--- Algorithms/InsertionSort.java
+++ Algorithms/InsertionSort.java
@@ -3,31 +3,33 @@ package Algorithms;
 import java.util.Arrays;
 
 public class InsertionSort {
-    static void main(String[] args) {
+    public static void main(String[] args) {
 //        int[] data = new int[]{-5, 20, 3, 9, 4, -4, 7, 2, 0, 45, 5, 8, 14, 5, 8, 4, 52};
-        int[] data = new int[]{-5, 9999, 20, 3, 0, 9, 4, -4, -999};
+        int[] data = new int[]{-5, 20, 0, -999, 3, 9, 4, -4};
         System.out.println(STR."Before = \{Arrays.toString(data)}");
         insertionSort(data);
         System.out.println(STR."After = \{Arrays.toString(data)}");
     }
 
-    private static void swap(int[] data, int a, int b) {
-        int temp = data[a];
-        data[a] = data[b];
-        data[b] = temp;
+    private static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+
     }
 
     private static void insertionSort(int[] data) {
-        for (int pick_one = 1; pick_one < data.length; pick_one++) {
-            int perfect_location = -1, back_tracker;
-            for (back_tracker = pick_one - 1; back_tracker >= 0; back_tracker--) {
-                if (data[pick_one] < data[back_tracker]) {
-                    perfect_location = back_tracker;
+        for (int i = 0; i <= data.length; i++) { // i <= data.length
+            if (data.length == 1) {
+                break;
+            }
+            for (int j = i - 1; j > 0; j--) {
+                if (data[j] < data[j - 1]) {
+                    swap(data, j, j - 1);
                 }
             }
-            if (perfect_location != -1) {
-                swap(data, pick_one, perfect_location);
-            }
         }
+
+
     }
-}
+}// DONE
