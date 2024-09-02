e147634c1b6820f26aeef15679734f8e3dd7b908
Kazi Rifat Morshed <kazirifat03.krm@gmail.com>
2024-09-02 23:38:44 +0600

    Create Array_Game_Score.java

---
+++ DataStracture/Array_Game_Score.java
@@ -0,0 +1,72 @@
+package DataStracture;
+
+class GameEntry {
+    private String name;
+    private int score;
+
+    public GameEntry(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return STR."Player \{this.name} has scored \{this.score}";
+    }
+}
+
+class ScoreBoard {
+    private int numEntries;
+    private GameEntry[] board;
+
+    public ScoreBoard(int numEntries) {
+        this.numEntries = numEntries;
+        board = new GameEntry[this.numEntries];
+    }
+
+    void add(GameEntry e) {
+        int newScore = e.getScore();
+        if (numEntries < board.length || newScore > board[numEntries - 1].getScore()) {
+            if (numEntries < board.length) {
+                this.numEntries++;
+
+                int j = numEntries - 1;
+                while (j > 0 && board[j - 1].getScore() < newScore) {
+                    board[j] = board[j - 1];
+                    j--;
+                }
+                board[j] = e;
+            }
+        }
+    }
+
+    public GameEntry remove(int i) throws IndexOutOfBoundsException {
+        if (i < 0 || i >= numEntries) {
+            throw new IndexOutOfBoundsException(STR."Invalid index \{i}");
+        }
+        GameEntry temp = board[i];
+        for (int j = i; j < numEntries - 1; j++) {
+            board[j] = board[j++];
+        }
+        board[numEntries - 1] = null;
+        numEntries--;
+        return temp;
+    }
+}
+
